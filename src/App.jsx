909e4038fe22342997fcc937704a3de9d2fb843d
hansa <hansaverma175@gmail.com>
2026-03-12 19:42:56 +0530

    restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
-import Login from "./pages/Login";
-import Home from "./pages/Home";
-import Ai from "./pages/Ai";
-import Search from "./pages/Search";
+import React from './react';
 
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/ai" element={<Ai />} />
-        <Route path="/search" element={<Search />} />
-    
-      </Routes>
-    </BrowserRouter>
   );
 }
 
