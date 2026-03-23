54eb9309d1beadea3abd5bddac68b8d258552399
hansa <hansaverma175@gmail.com>
2026-03-23 06:23:09 +0530

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React from './react';
 
-
+import Landing from './landing';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
+      <Routes>
+        <Route path = "/" element = {<Landing />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/landing.jsx
@@ -0,0 +1,8 @@
+const landing = () => {
+   return(
+    <div>
+        <div className="main_bg" style = {{backgroundColor: "#152238", height: "100vh", width: "100vw"}}></div>
+    </div>
+);
+}
+export default landing;
